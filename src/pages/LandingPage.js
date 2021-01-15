c2dc0e6e658701ce5125d53a34b4bcd01670474c
Rivaldy Nur Fachriza <rivaldynf@gmail.com>
2021-01-15 22:01:29 +0700

    Update

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import Header from "parts/Header";
 import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
 
 import landingPage from "json/landingPage.json";
 
@@ -12,6 +13,7 @@ export default class LandingPage extends Component {
       <>
         <Header {...this.props}></Header>
         <Hero data={landingPage.hero} />
+        <MostPicked data={landingPage.hero} />
       </>
     );
   }
--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import ImageHero from "assets/images/img-hero.jpg";
 import ImageHero_ from "assets/images/img-hero-frame.jpg";
-import IconCities from "assets/images/icons/icon-cities.svg";
-import IconTraveler from "assets/images/icons/icon-traveler.svg";
-import IconTreasure from "assets/images/icons/icon-treasure.svg";
+import IconCities from "assets/images/icons/ic_cities.svg";
+import IconTraveler from "assets/images/icons/ic_traveler.svg";
+import IconTreasure from "assets/images/icons/ic_treasure.svg";
 
 import Button from "elements/Button";
 
